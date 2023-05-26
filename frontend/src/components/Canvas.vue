e56aae3ea05c5c3980e18463717e4964e62bdc87
Vimuth Mendis <vimuth.mendis@tradingscreen.com>
2023-05-26 14:45:15 +0800

    add custom logic to emit

--- frontend/src/components/Canvas.vue
+++ frontend/src/components/Canvas.vue
@@ -1,8 +1,8 @@
 
 <template>
-  <div class="greetings">
-    <div id="graph"></div>
-  </div>
+<div class="greetings">
+	<div id="graph"></div>
+</div>
 </template>
 
 <script>
@@ -10,78 +10,77 @@
 import ForceGraph from 'force-graph';
 
 function generateRingGraph(n) {
-  const nodes = [];
-  const links = [];
+	const nodes = [];
+	const links = [];
+	for (let i = 0; i < n; i++) {
+		const node = {
+			id: i + 1,
+			name: i + 1,
+			val: 5,
+			leader: false,
+			receives: 0,
+		};
+		nodes.push(node);
 
-  for (let i = 0; i < n; i++) {
-    const node = {
-      id: i + 1,
-      name: i + 1,
-	  color: true,
-      val: 5,
-    };
-    nodes.push(node);
+		if (i < n - 1) {
+			const link = {
+				source: i + 1,
+				target: i + 2,
+				value: 1
+			};
+			links.push(link);
+		} else {
+			const link = {
+				source: n,
+				target: 1,
+				value: 1
+			};
+			links.push(link);
+		}
+	}
 
-    if (i < n - 1) {
-      const link = {
-        source: i + 1,
-        target: i + 2,
-      };
-      links.push(link);
-    } else {
-      // Connect the last node with the first node to form a ring
-      const link = {
-        source: n,
-        target: 1,
-      };
-      links.push(link);
-    }
-  }
-
-  return {
-    nodes,
-    links,
-  };
+	return {
+		nodes,
+		links,
+	};
 }
 
 function render(algo, numNodes) {
 	console.log("Rendering");
 	console.log(algo, numNodes);
 	const g = generateRingGraph(numNodes);
-    const elem = document.getElementById("graph");
-    const graph = ForceGraph()(elem);
-    graph.graphData(g);
+	const { nodes, links } = g;
+	const elem = document.getElementById("graph");
+	const graph = ForceGraph()(elem);
+	graph.graphData(g);
 	graph.backgroundColor("#D0EAD8");
-	graph.nodeColor(node => node.color ? "#F99597" : "F99597");
+	graph.nodeColor(node => node.leader ? "#10C1D8" : "#F99597");
 	graph.linkWidth(4);
 	graph.zoom(1/numNodes + 4, 500)
-	// graph.nodeRelSize(5);
 
+	graph
+		.linkDirectionalParticleWidth(10)
+		.linkDirectionalParticleColor(link => "#02a392")
+		.linkDirectionalParticleSpeed(0.015);
 
-	// setInterval(() => {
-	// 	const { nodes, links } = Graph.graphData();
-	// 	const id = nodes.length;
-	// 	const target = Math.round(Math.random() * (id - 1));
-	// 	console.log(id, target);
-	// 	Graph.graphData({
-	// 	nodes: [...nodes, { id }],
-	// 	links: [...links, { source: id, target: target }]
-	// 	});
-	// }, 1000);
+	const emit = setInterval(() => {
+		console.log("Emitting");
+		const linkId = Math.floor(Math.random() * links.length);
+		const recv = links[linkId].target;
+		console.log(nodes, recv)
+		recv.receives += 1;
+		recv.leader = recv.receives > 4;
+		graph.emitParticle(links[linkId]);
 
-
-	function removeNode(node) {
-		let { nodes, links } = graph.graphData();
-		links = links.filter(l => l.source !== node && l.target !== node); // Remove links attached to node
-		nodes.splice(node.id, 1); // Remove node
-		nodes.forEach((n, idx) => { n.id = idx; }); // Reset node ids to array index
-		graph.graphData({ nodes, links });
-	}
+		if(nodes.reduce((count, node) => count + (node.leader ? 1 : 0), 0) === numNodes){
+			clearInterval(emit);
+		}
+	}, 1000);
 }
 
 export default {
 	methods: {
-		render: render
+		render: render,
 	}
 }
 
